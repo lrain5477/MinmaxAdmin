b8f380d6d94aff051f7ff92eb01c1e8851c6b977
Jeff <jeffxdev@gmail.com>
2018-05-11 00:10:25 +0800

    Add language translations

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,124 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the system text in admin page.
+    |
+    */
+
+    'header' => [
+        'menu' => 'Menu',
+        'profile' => 'Profile',
+        'language' => 'Language',
+        'account' => 'Account',
+        'login' => 'Login',
+        'logout' => 'Logout',
+    ],
+
+    'breadcrumbs' => [
+        'home' => 'Dashboard',
+    ],
+
+    'sidebar' => [
+        'home' => 'Dashboard',
+    ],
+
+    'login' => [
+        'title' => 'Login to Super Admin System',
+        'forget' => 'Forget password?',
+        'captcha' => 'Captcha',
+        'remember' => 'Remember me',
+        'login_submit' => 'Login',
+        'forget_submit' => 'Submit',
+        'back_button' => 'Back',
+        'info' => [
+            'login' => 'Please login to admin system. If you get some problem when you try to login, please contact service@e-creative.tw or get back password. Password would be sent to your account email address. Website powered by MINMAX.',
+            'forget' => 'Enter your Email address, and we will send a mail to you to reset password.',
+        ],
+    ],
+
+    'grid' => [
+        'title' => [
+            'action' => 'Action'
+        ],
+        'actions' => [
+            'view' => 'View',
+            'edit' => 'Edit',
+            'delete' => 'Delete',
+        ],
+        'selection' => [
+            'all_active' => 'All Status',
+            'all_result' => 'All Results',
+            'all_guard' => 'All Guards',
+            'all_rule' => 'All Rules',
+        ],
+        'back' => 'Back',
+        'root' => 'Root',
+        'next_layer' => 'Sub list',
+        'click_to_switch' => 'Click to switch',
+        'search' => 'Search',
+        'filter' => 'Filter',
+    ],
+
+    'form' => [
+        'view' => 'View',
+        'create' => 'Create',
+        'edit' => 'Edit',
+        'import' => 'Import',
+        'export' => 'Export',
+        'back_list' => 'Back',
+        'record' => 'Record',
+        'note' => 'Note',
+        'select_default_title' => 'Please select',
+        'password_build_auto' => 'Default password is 123456. System auto set.',
+        'select_all' => 'Select All',
+        'select_clear' => 'Clear Selected',
+        'fieldSet' => [
+            'default' => 'Default',
+            'information' => 'Information',
+            'media' => 'Media',
+            'advanced' => 'Advanced',
+            'permission' => 'Permission',
+            'seo' => 'SEO Setting',
+            'system_record' => 'System Record',
+        ],
+        'button' => [
+            'send' => 'Send',
+            'reset' => 'Reset',
+            'import' => 'Import',
+            'export' => 'Export',
+            'media_image' => 'Choose Image',
+            'media_file' => 'Choose File',
+        ],
+        'message' => [
+            'create_success' => 'Create a new data success.',
+            'create_error' => 'Failed to create a new data.',
+            'edit_success' => 'Update data success.',
+            'edit_error' => 'Failed to update data.',
+            'delete_success' => 'Delete data success。',
+            'delete_error' => 'Can not delete the data. Please try again.',
+            'delete_error_account_self' => 'You can not delete your account.',
+            'import_success' => 'Data import success.',
+            'import_error' => 'Failed to import data.',
+            'import_error_extension' => 'Extension of your file is invalid.',
+            'export_error' => 'Failed to export data. Please try again.',
+        ],
+        'elfinder' => [
+            'limit_title' => 'Over selected limit.',
+            'limit_text' => 'You can only select :limit file',
+            'limit_confirm_button' => 'I See',
+            'remove_title' => 'Delete Warning!',
+            'remove_text' => 'The file will be deleted.',
+            'remove_cancel_button' => 'Cancel',
+            'remove_confirm_button' => 'I\'m sure.',
+            'remove_success_title' => 'Completed!',
+            'remove_success_text' => 'The file is deleted!',
+        ],
+    ],
+
+];
---
+++ resources/lang/en/web.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Web (Frontend) Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the system text in admin page.
+    |
+    */
+
+    'header' => [
+        'menu' => 'Menu',
+        'language' => 'Language',
+        'account' => 'Account',
+        'logout' => 'Logout',
+    ],
+
+];
