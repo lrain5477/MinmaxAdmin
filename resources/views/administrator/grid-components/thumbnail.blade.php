4e31cf16198fe3065c7e83da6f034b5c25df8e0d
Jeffy <design27@e-creative.tw>
2018-05-14 10:23:54 +0800

    Update photo thumbnail with no image

--- resources/views/administrator/grid-components/thumbnail.blade.php
+++ resources/views/administrator/grid-components/thumbnail.blade.php
@@ -5,9 +5,18 @@
  * @var integer $size
  */
 ?>
+@if(!is_null($value) && $value !== '' && File::exists(public_path($value)))
 <a class="thumb" href="{{ asset($value) }}" data-fancybox="" data-caption="{{ $alt }}">
     <span class="imgFill imgLiquid_bgSize imgLiquid_ready"
           style="background: url('{{ route('administrator.thumbnail', ['width' => $size, 'height' => $size, 'imagePath' => $value]) }}') center center no-repeat; background-size: cover;">
         <img src="{{ route('administrator.thumbnail', ['width' => $size, 'height' => $size, 'imagePath' => $value]) }}" alt="{{ $alt }}" style="display: none;">
     </span>
 </a>
+@else
+<div class="thumb">
+    <span class="imgFill imgLiquid_bgSize imgLiquid_ready"
+          style="background: url('{{ asset('admin/images/noimage.gif') }}') center center no-repeat; background-size: cover;">
+        <img src="{{ asset('admin/images/noimage.gif') }}" alt="" style="display: none;">
+    </span>
+</div>
+@endif
