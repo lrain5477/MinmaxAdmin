67e5aed2815b5f15279ad89cd2579d2b9c0cded5
Jeffy <design27@e-creative.tw>
2018-05-21 15:37:02 +0800

    Update CKEditor toolbar buttons

--- public/components/ckeditor/config.js
+++ public/components/ckeditor/config.js
@@ -5,30 +5,25 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	  config.toolbar = [['Source'],
-                      ['Undo', 'Redo', '-', 'SelectAll', 'RemoveFormat'],
-                      ['Format', 'Font', 'FontSize'],
-                      ['TextColor', 'BGColor'],
-                      ['Maximize', 'ShowBlocks', '-', 'About'], '/',
-                      ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript'],
-                      ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv'],
-                      ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
-                      ['Link', 'Unlink', 'Anchor'],
-                      ['Newplugin', 'Image', 'Table', 'HorizontalRule', 'SpecialChar'],
-                      ['Code']];
-    config.extraPlugins = 'newplugin';
+    config.toolbar = [
+        ['Source'],
+        ['Undo', 'Redo', '-', 'RemoveFormat'],
+        ['Format', 'Font', 'FontSize'],
+        ['TextColor', 'BGColor'],
+        ['Maximize', 'ShowBlocks'], '/',
+        ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript'],
+        ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'CreateDiv'],
+        ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+        ['Link', 'Unlink'],
+        ['Image', 'Table', 'HorizontalRule', 'SpecialChar'],
+        ['Code']
+    ];
 
-    config.removeButtons = 'Anchor';
     config.allowedContent = true;
     config.disallowedContent = 'img{width,height};img[width,height]';
     config.baseFloatZIndex = 10000000;
     config.enterMode = CKEDITOR.ENTER_BR;
     config.shiftEnterMode = CKEDITOR.ENTER_P;
     config.htmlEncodeOutput = false;
-
-
-    //config.filebrowserImageBrowseUrl = '/siteadmin/Files/Mag';
     config.font_names = 'Open Sans;Roboto Slab;Rokkitt;Arial;Arial Black;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana';
 };
